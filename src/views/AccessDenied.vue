7f2dce6481c031ffcdc8d41a25e0180f927e8be0
Tanran-Liu <142192793+Tanran-Liu@users.noreply.github.com>
2024-10-20 05:11:47 +1100

    16

--- src/views/AccessDenied.vue
+++ src/views/AccessDenied.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mt-5">
     <h1 class="text-center">Access Denied</h1>
-    <p class="text-center">aaaaaaaaaaaaaa?.</p>
+    <p class="text-center">aaaaaaaaaaaaaaa?.</p>
   </div>
 </template>
 
