a92c7068ad1f7f4732ee9c15268d0300c609e66c
Tanran-Liu <142192793+Tanran-Liu@users.noreply.github.com>
2024-10-20 04:10:19 +1100

    10

--- src/views/AccessDenied.vue
+++ src/views/AccessDenied.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container mt-5">
-      <h1 class="text-center">Access Denied</h1>
-      <p class="text-center">aaaaaaaaaaaaa?.</p>
-    </div>
-  </template>
-  
-  <script setup></script>
+  <div class="container mt-5">
+    <h1 class="text-center">Access Denied</h1>
+    <p class="text-center">aaaaaaaaaaaaaa?.</p>
+  </div>
+</template>
+
+<script setup></script>
