4c393ec17937cb36533de49880283899d091b16a
Tanran-Liu <142192793+Tanran-Liu@users.noreply.github.com>
2024-10-20 16:22:55 +1100

    assets to dist change vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/a/',  // 设置 GitHub Pages 的子路径
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,4 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
