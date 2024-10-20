f7d37f66516eea369e615788e3ef278f92b41da3
Tanran-Liu <142192793+Tanran-Liu@users.noreply.github.com>
2024-10-20 17:24:16 +1100

    change to createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -72,7 +72,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes // 传递路由配置
 });
 
