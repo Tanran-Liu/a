ea75615db3b1a943b401458ede8dcf59830a7477
Tanran-Liu <142192793+Tanran-Liu@users.noreply.github.com>
2024-10-20 15:42:52 +1100

    18

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -3,7 +3,7 @@
     <div>
       <h1>WEATHER APP</h1>
       <div class="search-bar">
-        <input type="text" v-model="city" placeholder="Enter city name" class="search-input" />
+        <input type="text" v-model="city" placeholder="qEnter city name" class="search-input" />
         <button @click="searchByCity" class="search-button">Search</button>
       </div>
     </div>
